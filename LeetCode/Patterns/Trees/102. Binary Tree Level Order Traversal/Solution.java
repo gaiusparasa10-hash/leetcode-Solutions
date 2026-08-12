9e63abc25b5f51fbeaaafd3496582080bd16bbe0
gaiusparasa10-hash <gaiusparasa10@gmail.com>
2026-08-12 16:25:19 +0530

    Add Java Solution to Patterns/Trees

---
+++ LeetCode/Patterns/Trees/102. Binary Tree Level Order Traversal/Solution.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+       List<List<Integer>> ans=new ArrayList();
+       if(root==null)
+       return ans;
+       Deque<TreeNode> q=new ArrayDeque();
+       q.offer(root);
+       while(!q.isEmpty())
+       {
+        List<Integer> l1=new ArrayList();
+        int size=q.size();
+        for(int i=0;i<size;i++)
+        {
+            TreeNode curr=q.poll();
+            l1.add(curr.val);
+            if(curr.left!=null)
+             q.offer(curr.left);
+             if(curr.right!=null)
+             q.offer(curr.right);
+        }
+        ans.add(l1);
+       }
+       return ans;
+    }
+}
