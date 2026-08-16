82ee0ca7d05a30794ea8194cff2679ad8018a174
gaiusparasa10-hash <gaiusparasa10@gmail.com>
2026-08-16 12:44:00 +0530

    Add Easy Java Solution from LeetCode

--- LeetCode/Java/Easy/9. Palindrome Number/Solution.java
+++ LeetCode/Java/Easy/9. Palindrome Number/Solution.java
@@ -2,13 +2,13 @@ class Solution {
     public boolean isPalindrome(int x) {
         int rev = 0;
         int n;
-        int t = x;
+        int tmp = x;
         while(x > 0){
             n = x % 10;
             rev = rev * 10 + n;
             x = x / 10;
         }
-        if(rev == t)
+        if(rev == tmp)
             return true;
         return false;
     }
