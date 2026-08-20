df561cf0921fa9c1ea5868a3e8ed3da04c9bded4
gaiusparasa10-hash <gaiusparasa10@gmail.com>
2026-08-20 11:05:40 +0530

    Add Easy Java Solution from LeetCode

--- LeetCode/Java/Easy/228. Summary Ranges/Solution.java
+++ LeetCode/Java/Easy/228. Summary Ranges/Solution.java
@@ -8,7 +8,6 @@ class Solution {
         while (i < nums.length) {
             int start = nums[i];
 
-            // Find the end of the current consecutive range
             while (i + 1 < nums.length &&
                    nums[i + 1] == nums[i] + 1) {
                 i++;
@@ -16,16 +15,12 @@ class Solution {
 
             int end = nums[i];
 
-            // Single number
             if (start == end) {
                 result.add(String.valueOf(start));
             } 
-            // Range of multiple numbers
             else {
                 result.add(start + "->" + end);
             }
-
-            // Move to the next range
             i++;
         }
 
